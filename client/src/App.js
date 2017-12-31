37d829ac729fb9236b1208e1a935b398aeff6896
Joseph Cha <josephcha4@gmail.com>
2017-12-31 21:11:33 +1100

    create main app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,46 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Grid, Container, Header, Icon } from "semantic-ui-react";
+import Cards from "./Cards";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    hazards: []
+  };
+
+  fetchAPI() {
+    fetch("/api")
+      .then(response => response.json())
+      .then(val => {
+        const parsed = JSON.parse(val);
+        this.setState({
+          hazards: parsed.features
+        });
+      });
+  }
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
   render() {
+    const options = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
+    const date = new Date().toLocaleString("en-AU", options);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid padded>
+        <Grid.Column>
+          <Container>
+            <Header as="h1" color="red">
+              <Icon name="warning sign" />
+              <Header.Content>
+                Live NSW road incidents &ndash; {date}
+              </Header.Content>
+            </Header>
+            <Cards data={this.state.hazards} />
+          </Container>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
