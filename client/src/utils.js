6634066c5f379d7684bb1c4b1e0f1d4640bbe0ea
Joseph Cha <josephcha4@gmail.com>
2018-01-02 19:22:41 +1100

    add more util functions

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export function convertUnixTime(ms) {
   const sanitize = ms.toString().substring(0, 10);
   const date = new Date(sanitize * 1000);
@@ -8,10 +10,41 @@ export function getTimeFrame(start, end) {
   if (!start) {
     return false;
   }
-
   const options = { year: 'numeric', month: 'short', day: 'numeric' };
   const startTime = convertUnixTime(start).toLocaleString('en-AU', options);
   const endTime = convertUnixTime(end).toLocaleString('en-AU', options);
+  return `${startTime} - ${endTime}`;
+}
 
-  return `${startTime} - ${endTime}`
+export function isNotEmpty(val) {
+  if (val !== undefined && val !== ' ' && val !== null) {
+    return true;
+  }
+  return false;
+}
+
+export function attendingGroups(attGroups) {
+  let groups = [];
+  attGroups.forEach(val => {
+    if (isNotEmpty(val)) {
+      groups.push(val);
+    }
+  });
+  const Markup = () =>
+    <div className="other-information">
+      Attending groups: {groups.join(', ')}
+    </div>;
+  return groups.length > 0 ? <Markup /> : '';
+}
+
+export function affectedTraffic(data) {
+  let direction;
+  if (data.impactedLanes[0]) {
+    direction = data.impactedLanes[0].affectedDirection;
+  }
+  const Markup = () =>
+    <div className="other-information">
+      Affected direction: {direction}
+    </div>;
+  return isNotEmpty(direction) ? <Markup /> : '';
 }
