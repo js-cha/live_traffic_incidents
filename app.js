477a2923d95f3e8e7256ad7a21960ac8fcf85463
Joseph Cha <josephcha4@gmail.com>
2018-01-04 16:28:19 +1100

    enable compression

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression')
 
 var api = require('./routes/api');
 
@@ -15,6 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 
 app.use('/', express.static(path.join(__dirname, 'client/build')));
 app.use('/api', api);
