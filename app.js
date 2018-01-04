b5ea4f590830b10ffc1c56c37abef25ef2ca457e
Joseph Cha <josephcha4@gmail.com>
2018-01-04 14:07:43 +1100

    try changing order

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var path = require('path');
 
 /* GET home page. */
-router.get('/', express.static(path.join(__dirname, 'client/build')));
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
