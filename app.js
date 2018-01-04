78d25350ce8946f372db3ea8332892319284f877
Joseph Cha <josephcha4@gmail.com>
2018-01-04 13:53:23 +1100

    serve static files from client/build - root

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', index);
 app.use('/users', users);
