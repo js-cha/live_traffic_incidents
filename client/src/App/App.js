966c3f304dc355a98c69814d7117db99bd59773c
Joseph Cha <josephcha4@gmail.com>
2018-01-05 20:48:54 +1100

    rewrite App component

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import ContentLoader from 'react-content-loader'
 import Cards from '../Cards/Cards';
 import MapWithAMarker from '../GoogleMaps/GoogleMaps';
 import { fetchData, sortData } from '../utils/utils';
 import './App.css';
-
-const MyLoader = () => <ContentLoader type="facebook" />
+import './normalize.css';
 
 class App extends Component {
   state = {
@@ -41,39 +39,57 @@ class App extends Component {
     const options = {
       weekday: 'long',
       year: '2-digit',
-      month: 'short',
+      month: 'long',
       day: 'numeric'
     };
     const date = new Date().toLocaleString('en-AU', options);
-    const combined = [].concat(this.state.hazards.accidents, this.state.hazards.ongoing, this.state.hazards.rest);
+    const { accidents, ongoing, rest } = this.state.hazards;
+    const combined = [].concat(accidents, ongoing, rest);
+
     return (
       <div>
         <header>
           <div className="container">
             <h1>NSW | Current Road Hazards</h1>
+            <span>{date}</span>
           </div>
         </header>
         <main>
-          <section>
-            <article>
-              <Cards data={this.state.hazards.accidents} />
-              <Cards data={this.state.hazards.rest} />
-              <Cards data={this.state.hazards.ongoing} />
-            </article>
-            <article>
-              <MapWithAMarker
-                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwLOREvY7B0YO5OZAqBiydgaGkRavgTTI"
-                loadingElement={<div style={{ height: `100%` }} />}
-                containerElement={<div style={{ height: `600px` }} />}
-                mapElement={<div style={{ height: `100%` }} />}
-                data={combined}
-                defaultCenter={this.state.coords}
-                defaultZoom={this.state.zoom}
-                coords={this.state.coords}
-                zoom={this.state.zoom}
-              />
-            </article>
-          </section>
+          <div className="container">
+            <section>
+              <article>
+                <div className="cards_container">
+                  <div className="cards accidents">
+                    <h2 className="cards__title">Accidents ({accidents.length})</h2>
+                    <Cards data={accidents} clickHandler={this.goToCoords.bind(this)} />
+                  </div>
+                  <div className="cards hazards">
+                    <h2 className="cards__title">Hazards ({rest.length})</h2>
+                    <Cards data={rest} clickHandler={this.goToCoords.bind(this)} />
+                  </div>
+                  <div className="cards ongoing">
+                    <h2 className="cards__title">Ongoing ({ongoing.length})</h2>
+                    <Cards data={ongoing} clickHandler={this.goToCoords.bind(this)} />
+                  </div>
+                </div>
+              </article>
+              <article>
+                <div id="google-maps">
+                  <MapWithAMarker
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwLOREvY7B0YO5OZAqBiydgaGkRavgTTI"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `600px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                    data={combined}
+                    defaultCenter={this.state.coords}
+                    defaultZoom={this.state.zoom}
+                    coords={this.state.coords}
+                    zoom={this.state.zoom}
+                  />
+                </div>
+              </article>
+            </section>
+          </div>
         </main>
         <footer>
         </footer>
@@ -83,30 +99,3 @@ class App extends Component {
 }
 
 export default App;
-
-{/*<Grid container>
-  <Grid.Column>
-    <Header as="h1" color="red">
-      <Icon name="warning sign" />
-      <Header.Content>
-        Live NSW road incidents &ndash; {date}
-      </Header.Content>
-    </Header>
-    <Cards data={this.state.hazards} clickHandler={this.goToCoords.bind(this)} />
-    <Card fluid id="google-maps">
-      <Card.Content>
-        <MapWithAMarker
-          googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCwLOREvY7B0YO5OZAqBiydgaGkRavgTTI"
-          loadingElement={<div style={{ height: `100%` }} />}
-          containerElement={<div style={{ height: `600px` }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-          data={this.state.hazards}
-          defaultCenter={this.state.coords}
-          defaultZoom={this.state.zoom}
-          coords={this.state.coords}
-          zoom={this.state.zoom}
-        />
-      </Card.Content>
-    </Card>
-  </Grid.Column>
-</Grid>*/}
