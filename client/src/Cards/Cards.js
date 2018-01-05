49631b5382ccfc117f0ad87ca075bd8d0c8a367b
Joseph Cha <josephcha4@gmail.com>
2018-01-05 20:49:26 +1100

    rewrite Cards component

--- client/src/Cards/Cards.js
+++ client/src/Cards/Cards.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactHtmlParser from 'react-html-parser';
+import ContentLoader from 'react-content-loader'
 import { convertUnixTime, getTimeFrame, isNotEmpty, attendingGroups, affectedTraffic } from '../utils/utils';
+import './Cards.css'
+
+const MyLoader = () => <ContentLoader type="facebook" />
 
 const Cards = props => {
+  if (props.data.length < 1) {
+    return <MyLoader />
+  }
+
   const CardList = props.data.map(function(currItem, index, array) {
-    /*
-    ** A lot of data transformation and checking for empty values.
-    */
     const allProps = currItem.properties;
     const roadProps = allProps.roads[0];
     const startedAt = `Started today at: ${convertUnixTime(allProps.created).toLocaleTimeString('en-US')}`;
     const timeframe = getTimeFrame(allProps.start, allProps.end) || startedAt;
     const aGroups = attendingGroups(allProps.attendingGroups);
     const aTraffic = affectedTraffic(roadProps);
+    const OtherAdvice = () => <div className="hazard__otherinfo">{ReactHtmlParser(allProps.otherAdvice)}</div>
     const coords = {
       lat: currItem.geometry.coordinates[1],
       lng: currItem.geometry.coordinates[0]
@@ -22,34 +28,36 @@ const Cards = props => {
     return (
       <div
         key={index}
-        className="hazards__hazard"
+        className="card"
       >
-        <div>
-          <strong>{roadProps.suburb}</strong>, {roadProps.mainStreet} <em>{roadProps.locationQualifier}</em>{' '}
-          {roadProps.crossStreet} {isNotEmpty(roadProps.secondLocation) ? `and ${roadProps.secondLocation}` : ''}
+        <div className="card__inner">
+          <div className="card__title">
+            <strong>{roadProps.suburb}</strong>, {roadProps.mainStreet} <em>{roadProps.locationQualifier}</em>{' '}
+            {roadProps.crossStreet} {isNotEmpty(roadProps.secondLocation) ? `and ${roadProps.secondLocation}` : ''}
+          </div>
+          <div className="card__information">
+            <div className="card__description">{allProps.displayName}</div>
+            {aTraffic}
+            {aGroups}
+            {isNotEmpty(allProps.otherAdvice) ? <OtherAdvice /> : ''}
+          </div>
         </div>
-        <div>
-          <div>{allProps.displayName}</div>
-          {aTraffic}
-          {aGroups}
-          <div>{ReactHtmlParser(allProps.otherAdvice)}</div>
-        </div>
-        <div>
+        <div className="card__misc">
           {timeframe}
+          <div className="card__misc__clickhandler" onClick={() => props.clickHandler(coords)}>
+            View on map
+          </div>
         </div>
       </div>
     );
   });
 
-  return (
-    <div className="hazards">
-      {CardList}
-    </div>
-  );
+  return [...CardList];
 };
 
 Cards.propTypes = {
-  data: PropTypes.array
+  data: PropTypes.array,
+  title: PropTypes.string
 };
 
 export default Cards;
